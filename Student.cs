a36cbbea448d6d4441f532cfb5c2228de00d7f9d
sailakshmiryali <105211826+sailakshmiryali@users.noreply.github.com>
2024-07-14 22:22:23 +0530

    Add files via upload
    This is my new project for creating SchoolInfo

---
+++ Student.cs
@@ -0,0 +1,10 @@
+﻿namespace Schoolinfo.Model
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Email {  get; set;}
+    }
+}
---
+++ StudentContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Schoolinfo.Model;
+
+namespace Schoolinfo.Data
+{
+    public class StudentContext:  DbContext
+    {
+        public StudentContext(DbContextOptions options)
+        : base(options) 
+        {
+        }
+
+            public DbSet<Student> Students { get; set; }
+        }
+}
